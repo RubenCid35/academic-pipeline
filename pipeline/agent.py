64ee1c1f19ba65d8ab4cc1b38917bca1556aaf74
Rubén Cid <rubencid001@gmail.com>
2025-01-22 23:06:36 +0100

    feat: added research information extraction step

--- pipeline/agent.py
+++ pipeline/agent.py
@@ -11,6 +11,7 @@ from langchain_core.messages import BaseMessage
 # graph nodes
 from pipeline.process.ingestion import ingest_pdf
 from pipeline.process.metadata  import extract_metadata
+from pipeline.process.research  import extract_research
 
 # instantiate the workflow
 pipeline_flow = Graph()
@@ -18,11 +19,13 @@ pipeline_flow = Graph()
 # add the workflow steps
 pipeline_flow.add_node('ingestion', ingest_pdf)
 pipeline_flow.add_node('metadata-extraction', extract_metadata)
+pipeline_flow.add_node('research-extraction', extract_research)
 
 # order steps
 pipeline_flow.add_edge(START, 'ingestion')
 pipeline_flow.add_edge('ingestion', 'metadata-extraction')
-pipeline_flow.add_edge('metadata-extraction', END)
+pipeline_flow.add_edge('metadata-extraction', 'research-extraction')
+pipeline_flow.add_edge('research-extraction', END)
 
 # Compile the graph
 graph = pipeline_flow.compile()
---
+++ pipeline/process/research.py
@@ -0,0 +1,59 @@
+from pipeline.process.utils import __get_llm
+from langgraph.graph import MessagesState
+
+from langchain_core.prompts import PromptTemplate
+from langchain_core.output_parsers import JsonOutputParser
+
+from pydantic import BaseModel, Field
+
+# ---------------------------------------------
+# Metadata Format
+# ---------------------------------------------
+class ResearchInformation(BaseModel):
+    research: list[str] = Field(description="list of key research findings")
+    methodology: str = Field(description="description of the methodology that is described in the publication.")
+
+parser = JsonOutputParser(pydantic_object=ResearchInformation)
+# ---------------------------------------------
+# Metadata Extraction
+# ---------------------------------------------
+RESEACH_EXTRACTION_TEMPLATE: str = """
+Given the following academic publication content, you need to list all the key findings of the research and explain
+the methodology that the authors used to discover them.. 
+
+{format_instructions}
+
+{content}
+"""
+
+RESEACH_EXTRACTION_PROMPT = PromptTemplate(
+    template=RESEACH_EXTRACTION_TEMPLATE,
+    input_variables = ["content"],
+    partial_variables={
+        "format_instructions": parser.get_format_instructions()
+    }
+)
+
+
+def extract_research(state: dict[str, str]) -> dict[str, str]:
+    """Research Information Extraction
+
+    Using an LLM, this function extracts the methodology and key findings of a research publication.
+
+    Args:
+        state (dict[str, str]): state of the agent. It requires the field `content` with the pdf content as str.
+
+    Returns:
+        dict[str, str]: all metadata information and content.
+    """
+    # build processing steps
+    llm   = __get_llm()
+    chain = RESEACH_EXTRACTION_PROMPT | llm | parser
+
+    # extract the required metadata
+    research: dict[str, str] = chain.invoke({ 'content': state['content']})
+
+    # add previous findings
+    research.update(state)
+    del research['content']
+    return research
